f1398558e113e7b3cbfc2e5d6012494650b3df0d
Noel Forte <noel@forte.is>
2024-06-01 17:19:30 -0400

    add eslint and editorconfig

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+import js from '@eslint/js';
+
+// Configs
+import gitignore from 'eslint-config-flat-gitignore';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+// Plugins
+import globals from 'globals';
+
+export default [
+	// Set ignores from gitignore
+	gitignore(),
+
+	// Import configuration presets
+	js.configs.recommended,
+	eslintConfigPrettier,
+
+	// Configure ignores
+	{
+		ignores: ['**/.cache', '**/_site', '**/node_modules', '**/functions'],
+	},
+
+	// Configure defaults
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+		},
+		rules: {
+			'no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+				},
+			],
+			'no-else-return': [
+				'error',
+				{
+					allowElseIf: false,
+				},
+			],
+			'prefer-regex-literals': ['error', { disallowRedundantWrapping: true }],
+			'prefer-object-spread': 'error',
+			'prefer-const': ['error', { destructuring: 'all' }],
+			'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+			'prefer-destructuring': [
+				'error',
+				{
+					VariableDeclarator: { array: false, object: true },
+					AssignmentExpression: { array: false, object: false },
+				},
+			],
+			'object-shorthand': ['error', 'always'],
+			'arrow-body-style': ['error', 'as-needed'],
+			eqeqeq: ['error', 'smart'],
+		},
+	},
+
+	// Match all node-files
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+];
