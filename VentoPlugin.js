597859b9de4c2796d6651cd5e651f08fd6e0b560
Noel Forte <noel@forte.is>
2024-06-01 17:20:11 -0400

    add more typings

--- VentoPlugin.js
+++ VentoPlugin.js
@@ -1,5 +1,14 @@
 /**
  * @file Adds Vento support to eleventy.
+ *
+ * @typedef VentoPluginOptions
+ * @prop {boolean} retrieveGlobals If true, uses
+ * [`node-retrieve-globals`](https://www.npmjs.com/package/node-retrieve-globals)
+ * to merge globals back into the Data Cascade outside of front matter.
+ * @prop {Object<string,(...any) => any} filters An object containing methods that
+ * will be loaded as filters into Vento.
+ * @prop {import('ventojs').Options} ventoOptions Vento engine configuration object
+ * that will be merged with default options.
  */
 
 import path from 'node:path';
@@ -9,25 +18,32 @@ import VentoJs from 'ventojs';
 
 /**
  * @param {import('@11ty/eleventy/src/UserConfig.js').default} eleventyConfig
+ * @param {VentoPluginOptions} options
  */
-export function VentoPlugin(eleventyConfig, pluginOptions = {}) {
-	const { directories } = eleventyConfig;
+export function VentoPlugin(eleventyConfig, options = {}) {
+	const { dir } = eleventyConfig;
 
-	const env = VentoJs({
-		includes: directories.includes,
-	});
+	options = {
+		retrieveGlobals: false,
+		filters: {},
+		ventoOptions: {
+			includes: path.join(dir.input, dir.includes),
+			autoescape: false,
+		},
+		...options,
+	};
 
 	const jsHelpers = eleventyConfig.javascriptFunctions;
 
-	eleventyConfig.addTemplateFormats('vto');
+	const env = VentoJs(options.ventoOptions);
 
+	env.cache.clear();
+
+	eleventyConfig.addTemplateFormats('vto');
 	eleventyConfig.addExtension('vto', {
 		outputFileExtension: 'html',
 
-		async compile(inputContent, inputPath) {
-			env.cache.clear();
-			const template = await env.load(inputPath);
-
+		async compile(inputContent) {
 			return async (data) => {
 				for (const [name, callback] of Object.entries(jsHelpers)) {
 					jsHelpers[name] = callback.bind(data);
@@ -40,10 +56,15 @@ export function VentoPlugin(eleventyConfig, pluginOptions = {}) {
 		},
 
 		async getData(inputPath) {
+			if (!options.retrieveGlobals) return {};
+
 			const { code } = await env.load(inputPath);
 
 			const vm = new RetrieveGlobals(code);
-			const globals = await vm.getGlobalContext({ __exports: '', it: {} });
+			const globals = await vm.getGlobalContext({
+				__exports: '',
+				[options.ventoOptions?.dataVarname || 'it']: {},
+			});
 
 			return globals;
 		},
