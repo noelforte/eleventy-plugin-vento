58d5431ef14b00895e1d9e1440a3d9887da7fc14
Noel Forte <noel@forte.is>
2024-06-01 17:20:20 -0400

    feat: document more options, add namespacing to helpers

--- VentoPlugin.js
+++ VentoPlugin.js
@@ -2,10 +2,14 @@
  * @file Adds Vento support to Eleventy.
  *
  * @typedef VentoPluginOptions
- * @prop {Object<string,(...any) => any} filters An object containing methods that
+ * @prop {Object<string,(...any) => any} [filters={}] An object containing methods that
  * will be loaded as filters into Vento.
- * @prop {import('ventojs').Options} ventoOptions Vento engine configuration object
+ * @prop {import('ventojs').Options} [ventoOptions] Vento engine configuration object
  * that will be merged with default options.
+ * @prop {string|boolean} [addHelpers=true] Whether
+ * [Javascript Functions](https://www.11ty.dev/docs/languages/javascript/#javascript-template-functions)
+ * should be merged into data provided to templates. If a string, functions will be namespaced under
+ * a property with this name.
  */
 
 import path from 'node:path';
@@ -16,10 +20,16 @@ import VentoJs from 'ventojs';
  * @param {VentoPluginOptions} options
  */
 export function VentoPlugin(eleventyConfig, options = {}) {
-	const { dir, javascriptFunctions } = eleventyConfig;
-	const env = VentoJs(options.ventoOptions);
+	if (!('addExtension' in eleventyConfig)) {
+		console.log(
+			`[eleventy-plugin-vento] WARN Eleventy plugin compatibility: Custom templates are required for this plugin, please use Eleventy v1.0 or newer.`
+		);
+	}
 
+	// Pull some bits from eleventyConfig and merge user-options with defaults
+	const { dir, javascriptFunctions } = eleventyConfig;
 	options = {
+		addHelpers: true,
 		filters: {},
 		ventoOptions: {
 			includes: path.join(dir.input, dir.includes),
@@ -28,18 +38,34 @@ export function VentoPlugin(eleventyConfig, options = {}) {
 		...options,
 	};
 
+	// Init vento
+	const env = VentoJs(options.ventoOptions);
+
+	// Load user-defined filters into vento
 	for (const filter in options.filters) env.filters[filter] = options.filters[filter];
 
+	// Clear out the cache if need be.
 	env.cache.clear();
 
+	// Add vto as a template format and create a custom template for it
 	eleventyConfig.addTemplateFormats('vto');
 	eleventyConfig.addExtension('vto', {
 		outputFileExtension: 'html',
 
 		async compile(inputContent) {
 			return async (data) => {
-				for (const jsHelper in javascriptFunctions) {
-					data[jsHelper] = javascriptFunctions[jsHelper].bind(data);
+				if (options.addHelpers) {
+					// Extract a possible namespace from the addHelpers option
+					const namespace = options.addHelpers;
+
+					// Rebind functions to page data so this.page, this.eleventy, etc works as intended
+					const helpers = Object.assign(
+						...Object.entries(javascriptFunctions).map(([name, fn]) => ({ [name]: fn.bind(data) }))
+					);
+
+					// Merge helpers into page data
+					if (typeof namespace === 'string') data = { ...data, [namespace]: helpers };
+					else data = { ...data, ...helpers };
 				}
 
 				return env.runString(inputContent, data).then(({ content }) => content);
