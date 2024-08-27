8246c40cf5408dadba39e5587a73e32787d6642f
Noel Forte <noel@forte.is>
2024-08-27 18:03:08 -0400

    test: add autotrim tests

--- tests/autotrim/autotrim.test.js
+++ tests/autotrim/autotrim.test.js
@@ -1,17 +1,23 @@
 import { expect, test } from 'vitest';
 import { runBuild } from '#test-instance';
 
-const [trimSingle, trimAll] = await Promise.all([
-	runBuild(import.meta.dirname, { vento: { trimTags: ['set'] } }),
-	runBuild(import.meta.dirname, { vento: { trimTags: true } }),
+const [trimSingle, trimAll, trimExtends] = await Promise.all([
+	runBuild(import.meta.dirname, { vento: { autotrim: { tags: ['set'] } } }),
+	runBuild(import.meta.dirname, { vento: { autotrim: true } }),
+	runBuild(import.meta.dirname, {}),
 ]);
 
 test('should trim a single tag', async () => {
 	const page = trimSingle[0];
-	await expect(page.content).toMatchFileSnapshot('./build/autotrim-single.html');
+	await expect(page.content).toMatchFileSnapshot('./__snapshots__/autotrim-single.html');
 });
 
 test('should trim all tags', async () => {
 	const page = trimAll[0];
-	await expect(page.content).toMatchFileSnapshot('./build/autotrim-all.html');
+	await expect(page.content).toMatchFileSnapshot('./__snapshots__/autotrim-all.html');
 });
+
+// test('should trim all tags with an extra option', async () => {
+// 	const page = trimExtends[0];
+// 	await expect(page.content).toMatchFileSnapshot('./__snapshots__/autotrim-extends.html');
+// });
