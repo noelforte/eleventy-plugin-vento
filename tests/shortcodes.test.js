51f65bcf2812dddf84cdc353691945322b920ca9
Noel Forte <noel@forte.is>
2024-09-18 22:13:17 -0400

    rewrite tests to support new shortcode functionality

--- tests/shortcodes.test.js
+++ tests/shortcodes.test.js
@@ -1,14 +1,31 @@
 import { EleventyTest } from './_eleventy-test.js';
 import { test } from 'vitest';
 
-const testRun = new EleventyTest('./shortcodes/');
-
-test('shortcode without argument', async ({ expect }) => {
-	const { content } = await testRun.getBuildResultForUrl('/basic/');
-	expect(content).toBe('Hello world!\n');
+const testRun = new EleventyTest('./shortcodes/', {
+	useEleventyConfig: true,
 });
 
-test('shortcode with arguments', async ({ expect }) => {
-	const { content } = await testRun.getBuildResultForUrl('/with-arguments/');
-	await expect(content).toMatchFileSnapshot('./_results/shortcodes-arguments.html');
+test('run single shortcodes', async ({ expect }) => {
+	const { content } = await testRun.getBuildResultForUrl('/single/');
+	await expect(content).toMatchFileSnapshot('./_results/shortcodes-single.html');
+});
+
+test('run single shortcodes with filters', async ({ expect }) => {
+	const { content } = await testRun.getBuildResultForUrl('/single-filters/');
+	await expect(content).toMatchFileSnapshot('./_results/shortcodes-single-filters.html');
+});
+
+test('run paired shortcodes', async ({ expect }) => {
+	const { content } = await testRun.getBuildResultForUrl('/paired/');
+	await expect(content).toMatchFileSnapshot('./_results/shortcodes-paired.html');
+});
+
+test('run paired shortcodes with filters', async ({ expect }) => {
+	const { content } = await testRun.getBuildResultForUrl('/paired-filters/');
+	await expect(content).toMatchFileSnapshot('./_results/shortcodes-paired-filters.html');
+});
+
+test('verify nested shortcodes and text can compile together', async ({ expect }) => {
+	const { content } = await testRun.getBuildResultForUrl('/paired-nested/');
+	await expect(content).toMatchFileSnapshot('./_results/shortcodes-paired-nested.html');
 });
--- tests/shortcodes/eleventy.config.js
+++ tests/shortcodes/eleventy.config.js
@@ -1,4 +1,37 @@
-export default function (eleventyConfig) {
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+export default function eleventy(eleventyConfig) {
+	// Shortcodes
 	eleventyConfig.addShortcode('helloWorld', () => 'Hello world!');
 	eleventyConfig.addShortcode('possumPosse', (number = 'the') => `Release ${number} possums!!!`);
+	eleventyConfig.addShortcode('pageUrlCompare', function (url) {
+		return this.page.url === url ? 'page matched' : "page didn't match";
+	});
+
+	eleventyConfig.addShortcode('types', (stringType, numberType, booleanType) => {
+		const checks = [
+			typeof stringType === 'string',
+			typeof numberType === 'number',
+			typeof booleanType === 'boolean',
+		];
+
+		const test = checks.every((check) => check === true);
+
+		return `${test}`;
+	});
+
+	eleventyConfig.addPairedShortcode(
+		'sectionWrap',
+		(content, classname) =>
+			`<section${classname ? ` class="${classname}"` : ''}>${content}</section>`
+	);
+
+	eleventyConfig.addPairedShortcode(
+		'codeBlock',
+		(content, languageID = 'text') =>
+			`<pre class="code-block"><code class="lang-${languageID}">${content}</code></pre>`
+	);
+
+	eleventyConfig.addPairedShortcode('appendPageUrl', function (content) {
+		return `${content}\n<span class="url">${this.page.url}</span>`;
+	});
 }
