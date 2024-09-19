5573b4bbdc5edac3ea0335a355b868fbf38d796c
Noel Forte <noel@forte.is>
2024-09-19 19:28:55 -0400

    update synchronization script

--- scripts/generate-changesets.js
+++ scripts/generate-changesets.js
@@ -1,37 +1,47 @@
 import { x } from 'tinyexec';
-import { readFile, writeFile } from 'node:fs/promises';
+import { readFile, writeFile, appendFile } from 'node:fs/promises';
 
+// Load data from package.json
 const pkg = await readFile('./package.json', 'utf8').then((data) => JSON.parse(data));
 
+// Declare latest tag
 const LATEST_TAG = await x('git', ['describe', '--tags', '--abbrev=0']).then(({ stdout }) =>
 	stdout.trim()
 );
 
 console.log(`Checking for renovate commits since ${LATEST_TAG} tag...\n`);
 
+// Loop over each dependency, grab latest commit and match if it was comitted by
 const writeChangesets = Object.keys(pkg.dependencies).map(async (dependencyName) => {
-	const { stdout: commit } = await x('git', [
+	const commit = await x('git', [
 		'log',
 		'-1',
+		'--author',
+		String.raw`renovate\[bot\]`,
 		'-G',
 		`"${dependencyName}":`,
-		'--format=commit %h%nauthor %an%n%n%B',
+		'--format=commit %h%n%B',
 		`${LATEST_TAG}..HEAD`,
 		'package.json',
 	]);
 
-	const match = /^commit (?<hash>[\da-f]+)\nauthor renovate\[bot]\n\n(?<body>[\S\s]+)$/gm.exec(
-		commit
-	);
+	const match = /^commit (?<hash>[\da-f]+)$\n(?<body>[\S\s]+)$/gm.exec(commit.stdout);
 
 	if (!match) {
 		console.log(`No renovate commits for '${dependencyName}'`);
+		await setOutput('changesets-created', false);
 		return;
 	}
 
-	const data = `---\neleventy-plugin-vento: minor\n---\n\n${match.groups.body}`;
+	console.log(`Found update for ${dependencyName} at ${match.groups.hash}`);
 
-	await writeFile(`./.changeset/renovate-${dependencyName}-${match.groups.hash}.md`, data, 'utf8');
+	await writeFile(
+		`./.changeset/z_dependencies_${dependencyName}.md`,
+		`---\neleventy-plugin-vento: minor\n---\n\n${match.groups.body}`,
+		'utf8'
+	);
+
+	await setOutput('changesets-created', true);
 
 	return true;
 });
@@ -40,3 +50,11 @@ await Promise.all(writeChangesets).then((writers) => {
 	const { length: files } = writers.filter(Boolean);
 	console.log(`\nGenerated ${files} changeset files.\n`);
 });
+
+async function setOutput(key, value) {
+	if (process.env.GITHUB_OUTPUT) {
+		await appendFile(process.env.GITHUB_OUTPUT, `${key}=${value}`);
+	} else {
+		process.stdout.write(`OUTPUT: ${key}=${value}\n`);
+	}
+}
