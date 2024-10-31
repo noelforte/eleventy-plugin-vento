28ea3648ace71ced8ad5890ac2111b5fe89a1f98
Noel Forte <noel@forte.is>
2024-10-31 14:17:43 -0400

    adjust github workflows, scripts, and build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: 'esm',
+	dts: true, // Generate declaration file (.d.ts)
+	splitting: false,
 	clean: true,
-	dts: true,
 });
