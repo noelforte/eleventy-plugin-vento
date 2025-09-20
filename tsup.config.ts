aeffdcecd56de2ce0af3d35222ac14700e370473
Noel Forte <noel@forte.is>
2025-09-20 18:27:52 -0400

    migrate to tsdown for builds

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-	entry: ['src/plugin.ts'],
-	format: 'esm',
-	dts: true, // Generate declaration file (.d.ts)
-	splitting: false,
-	clean: true,
-});
