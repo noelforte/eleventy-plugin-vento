c3379cbdd27dc7956210f3259449accdeecf5be6
Noel Forte <noel@forte.is>
2025-12-02 00:05:17 -0500

    format all files

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
 	project: ['**/*.{js,ts}', '!tests/stubs*/**'],
-	ignoreDependencies: ["prettier"]
+	ignoreDependencies: ['prettier'],
 };
 
 export default config;
