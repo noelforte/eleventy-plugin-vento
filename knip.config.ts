9ee1adf595f194df8355ec38c3c5a2c3f9fcb99c
Noel Forte <noel@forte.is>
2025-12-01 23:58:11 -0500

    prevent knip from catching prettier when executed by mise

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,7 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
 	project: ['**/*.{js,ts}', '!tests/stubs*/**'],
+	ignoreDependencies: ["prettier"]
 };
 
 export default config;
