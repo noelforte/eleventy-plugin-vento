a759c76a0a9d6430a4f1e080d2034df2d3e00cb6
Noel Forte <noel@forte.is>
2026-07-20 15:33:48 -0400

    initialize sampo config

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
 	project: ['**/*.{js,ts}', '!tests/stubs*/**'],
-	ignoreDependencies: ['prettier', 'oxlint'],
+	ignoreDependencies: ['prettier', 'oxlint', 'sampo'],
 };
 
 export default config;
