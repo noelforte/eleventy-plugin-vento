1129da627a58afdc6cc25dba6003903c2eca06eb
Noel Forte <noel@forte.is>
2026-07-28 04:14:34 -0400

    Revert "initialize sampo config"
    This reverts commit a759c76a0a9d6430a4f1e080d2034df2d3e00cb6.

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,7 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
 	project: ['**/*.{js,ts}', '!tests/stubs*/**'],
-	ignoreDependencies: ['prettier', 'oxlint', 'sampo'],
+	ignoreDependencies: ['prettier', 'oxlint'],
 };
 
 export default config;
